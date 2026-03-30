e7c5686cce31aef98c87decae7b471f0ae0c9196
MrcKlim <mrcchezerklim@gmail.com>
2026-03-30 19:02:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ var app = new Vue({
             {id: 1, title: 'White Cabbage', short_text: 'Fresh and crispy white cabbage', image: 'cab1.png', desc: 'Full description of white cabbage...'},
             {id: 2, title: 'Green Cabbage', short_text: 'Classic green cabbage, rich in vitamins', image: 'cab2.png', desc: 'Full description of green cabbage...'},
             {id: 3, title: 'Cauliflower', short_text: 'Tender cauliflower with mild flavor', image: 'cab3.png', desc: 'Full description of cauliflower...'},
-            {id: 4, title: 'Napa Cabbage', short_text: 'Sweet Chinese cabbage', image: 'cab4.png', desc: 'Full description of napa cabbage...'},
-            {id: 5, title: 'Brussels Sprouts', short_text: 'Nutritious miniature cabbages', image: 'cab5.png', desc: 'Full description of brussels sprouts...'}
+            {id: 4, title: 'Broccoli', short_text: 'Fresh green broccoli rich in nutrients', image: 'cab4.png', desc: 'Full description of broccoli...'},
+            {id: 5, title: 'Napa Cabbage', short_text: 'Sweet Chinese cabbage with tender leaves', image: 'cab5.png', desc: 'Full description of napa cabbage...'}
         ],
         product: {}, 
         btnVisible: 0
